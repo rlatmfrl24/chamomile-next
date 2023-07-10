b28a62d3cc63352b75223d5504c242678ad0184c
SulfurBottom <397love@gmail.com>
2023-07-10 10:07:57 +0900

    swiper element migration

--- src/app/project/[id]/project-preview.tsx
+++ src/app/project/[id]/project-preview.tsx
@@ -1,37 +1,29 @@
+"use client";
+
 import { NextPage } from "next";
-import { AspectRatio, Flex, Image } from "@chakra-ui/react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import "swiper/css/navigation";
-import "swiper/css";
+import { AspectRatio, Box, Flex, Image } from "@chakra-ui/react";
+import { register } from "swiper/element/bundle";
+register();
 
 const ProjectPreview: NextPage<{
   thumbs: string[];
 }> = ({ thumbs }) => {
   return (
     <AspectRatio ratio={16 / 9}>
-      <Flex>
-        <Swiper
-          className="h-full"
-          modules={[Navigation]}
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-        >
-          {thumbs.map((thumb) => (
-            <SwiperSlide key={thumb} className="flex">
-              <Flex className="h-full justify-center items-center">
-                <Image
-                  src={thumb}
-                  alt="Project Image Placeholder"
-                  objectFit="contain"
-                  className="h-full"
-                />
-              </Flex>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </Flex>
+      <swiper-container navigation>
+        {thumbs.map((thumb) => (
+          <swiper-slide key={thumb} className="flex">
+            <Flex className="h-full justify-center items-center">
+              <Image
+                src={thumb}
+                alt="Project Image Placeholder"
+                objectFit="contain"
+                className="h-full"
+              />
+            </Flex>
+          </swiper-slide>
+        ))}
+      </swiper-container>
     </AspectRatio>
   );
 };
