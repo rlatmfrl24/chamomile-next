c6b8b5313249d20541f8eb6f0ce8780593aa8c67
SulfurBottom <397love@gmail.com>
2023-07-10 10:29:56 +0900

    swiper 일부 Style 수정

--- src/app/project/[id]/project-preview.tsx
+++ src/app/project/[id]/project-preview.tsx
@@ -3,6 +3,7 @@
 import { NextPage } from "next";
 import { AspectRatio, Box, Flex, Image } from "@chakra-ui/react";
 import { register } from "swiper/element/bundle";
+import { CSSProperties } from "react";
 register();
 
 const ProjectPreview: NextPage<{
@@ -10,10 +11,19 @@ const ProjectPreview: NextPage<{
 }> = ({ thumbs }) => {
   return (
     <AspectRatio ratio={16 / 9}>
-      <swiper-container navigation>
+      <swiper-container
+        navigation
+        pagination
+        style={
+          {
+            "--swiper-navigation-size": "24px",
+            "--swiper-pagination-bullet-inactive-color": "#fff",
+          } as CSSProperties
+        }
+      >
         {thumbs.map((thumb) => (
           <swiper-slide key={thumb} className="flex">
-            <Flex className="h-full justify-center items-center">
+            <Flex className="h-full justify-center items-center p-10">
               <Image
                 src={thumb}
                 alt="Project Image Placeholder"
